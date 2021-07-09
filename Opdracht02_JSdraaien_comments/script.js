8e215eee3ddaecd970c7eb4a40ccbff5692f0e15
junerae <80211043+junerae@users.noreply.github.com>
2021-07-09 10:20:36 +0200

    Add files via upload

---
+++ Opdracht02_JSdraaien_comments/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten")
+
+// "Dit is een grote som"
+
+// let sum1 = 1230941 * 1823794;
+// console.log(sum1);
+
+// let sum2 = 637263 / 54;
+// console.log(sum2);
+
